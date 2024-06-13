a97c0e1d7e0ce2ad06303f1caf768e1ea8fc3a42
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-06-13 11:46:43 +0200

    Show usage when there is no command-line argument

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ mod segmented_sieve;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let limit: usize = if args.len() >= 2 {
-        args[1].parse().expect("Invalid number")
-    } else {
-        1_000_000
-    };
-
-    let count = segmented_sieve::segmented_sieve(limit);
+    if args.len() < 2 {
+        println!("Usage: {} STOP", args[0]);
+        println!("Counts the primes inside [2, STOP] (< 2^64)");
+        return;
+    }
+    let stop = args[1].parse().expect("STOP cannot be parsed");
+    let count = segmented_sieve::segmented_sieve(stop);
     println!("{} primes found.", count);
 }
